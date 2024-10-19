a3a1d656c0c3009e585a3aee185c7902abd773be
ramin <raminraad@outlook.com>
2024-10-20 00:13:02 +0330

    Refactor dependencies

--- src/Core/ESys.Application/Models/CalcFormFileUploadRequest.cs
+++ src/Core/ESys.Application/Models/CalcFormFileUploadRequest.cs
@@ -1,9 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
-
 namespace ESys.Application.Models;
 
 public class CalcFormFileUploadRequest
 {
     public string BizId { get; set; } = string.Empty;
     public string OrderId { get; set; } = string.Empty;
-}
+}           
